ce0192eba88396e84049209c3f1234dd133c859f
ᴍᴏʀɢᴀɴ <ali2763.mar@gmail.com>
2024-03-17 18:35:18 +0330

    create component LoginForm

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,19 +1,10 @@
-"use client"
+import LoginForm from "@/components/auth/LoginFrom";
 
-
-import { useFormState } from "react-dom";
-import SubmitButton from "@/components/SubmitButton";
-import { useEffect } from "react";
-import { toast } from "react-toastify";
-import { login } from "@/actions/auth";
 
 
 export default function LoginPage(){
 
-    const [stateLogin, formActionLogin] = useFormState(login, {});
-useEffect(() => {
-  toast(stateLogin?.message , {type: `${stateLogin?.status}`})
-},[stateLogin])
+ 
 
 
 
@@ -22,15 +13,16 @@ useEffect(() => {
         <div className="container">
             <div className="row mt-5">
                 <div className="col-md-4 offset-md-4">
+                   <LoginForm />
                     <div className="card">
                         <div className="card-body">
                             <div className="form_container">
-                                <form action={formActionLogin} >
+                                <form >
                                     <div className="mb-3">
-                                        <label className="form-label">شماره موبایل</label>
+                                        <label className="form-label">کد تایید</label>
                                         <input name="cellphone" type="text" className="form-control" />
                                     </div>
-                                    <SubmitButton title="ورود" style="btn btn-primary btn-auth" />
+                                    {/* <SubmitButton title="ورود" style="btn btn-primary btn-auth" /> */}
                                 </form>
                             </div>
                         </div>
