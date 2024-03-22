edb09dd5dffa5d1ea66af54e6a24763dbda70f9c
ᴍᴏʀɢᴀɴ <ali2763.mar@gmail.com>
2024-03-22 19:06:19 +0330

    create Middleware file for Redirect user to login

---
+++ middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server"
+
+export function middleware(req){
+    const token = req.cookies.get('token')
+    if(!token){
+        return NextResponse.redirect(new URL('/auth/login' , req.url))
+    }
+}
+export const config = {
+    matcher : ['/profile/:path*']
+}
